aebbb5e77f3949489f9012d07098898875671de6
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-30 18:19:29 +0200

    Includes

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    	  <div class="ui container">
+    	    <div class="footer-content">
+              <div class="copyright">©2018 <a href="">Gamaliel Rausch</a> All rights reserved</div>
+              <div class="right-content">
+                <div class="ui horizontal list">
+                  <a class="item">Support</a>
+                  <a class="item">Contact Us</a>
+                  <a class="item">Services</a>
+                  <a class="item">Reviews</a>
+                  <a class="item">Credits</a>
+                </div> <!-- list -->
+              </div>
+    	    </div> <!-- footer content -->
+    	  </div> <!-- ui container -->
+    	</footer>
+    	
+      </div> <!-- wrapper -->
+    </div> <!-- pusher -->
+  </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="core/semantic.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/search.js"></script>
+    <script src="js/slick.min.js"></script> 
+    <?php wp_footer(); ?>
+  </body>
+</html>
