da4121062b413357adbb89fdb1b29151252edf12
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-31 18:45:04 +0200

    Menus

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+function wptutsplus_register_theme_menu() {
+    register_nav_menu( 'primary', 'Main Navigation Menu' );
+}
+add_action( 'init', 'wptutsplus_register_theme_menu' );
+?>
--- includes/header.php
+++ includes/header.php
@@ -33,20 +33,47 @@
                   <a href="mailto:gamaliel.rausch@gmail.com?subject=Rausch Creative enquiry" class="item">gamaliel.rausch@gmail.com</a>
               </div> <!-- .left Menu -->
             </div> <!-- listing menut -->
-          <div class="primary-menu">
-              <div class="ui secondary inverted menu">
-               <div class="left menu">
-                <a class="toc item toogle-menu"><span></span><p>Menu</p></a>
-                <a href="index.php" class="logo"><img src="<?php echo get_bloginfo('template_url') ?>/assets/images/logo.png" width="100"  class="ui image"/></a>
-                </div>
-                <div class="right menu">
-                  <a href="index.php" class="active item">Home</a>
-                  <a href="about.php" class="item">About</a>
-                  <a href="work.php" class="item">My Work</a>
-                  <a href="Client.php" class="item">Clients</a>
-                  <a href="tel:015901076051" class="item">01591076041</a>
-                  <a href="mailto:gamaliel.rausch@gmail.com?subject=Rausch Creative enquiry" class="item">gamaliel.rausch@gmail.com</a>
-                </div>
-              </div>
-    		</div> <!-- site branding -->
+            
+
+          <?php
+
+$menu_name = 'primary'; // this is the registered menu name
+
+if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) :
+  $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+  $menu_items = wp_get_nav_menu_items($menu->term_id);
+  echo '<div class="primary-menu">';
+  echo '<div class="ui secondary inverted menu">';
+  echo '<div class="ui secondary inverted menu">';
+  echo '<div class="left menu">';
+  echo '<a class="toc item toogle-menu"><span></span><p>Menu</p></a>';
+  echo '<a href="index.php" class="logo"><img src="" width="100"  class="ui image"/></a>';
+  echo '</div>';
+  echo '<div class="right menu">';
+     echo'<a href="/index.php" class="active item">Home (Static Menu)</a>';
+     echo '<a href="about.php" class="item">About</a>';
+     echo '<a href="/work.php" class="item">My Work</a>';
+     echo '<a href="/client.php" class="item">Clients</a>';
+     echo '<a href="tel:015901076051" class="item">01591076041</a>';
+    echo ' <a href="mailto:gamaliel.rausch@gmail.com?subject=Rausch Creative enquiry" class="item">gamaliel.rausch@gmail.com</a>';
+   echo '</div>';
+ echo '</div>';
+    foreach ( (array) $menu_items as $key => $menu_item ) :
+      $title = $menu_item->title;
+      $url = $menu_item->url;
+      $class = $menu_item->classes; // get array with class names
+      if ( get_the_ID() == $menu_item->object_id ) { // check for current page
+        echo '<a class="active item" href="' . $url . '">';
+      } else {
+        echo '<a class="item" href="' . $url . '">';
+      } 
+        echo '<i class="item ' . implode(' ', $class) . '"></i>' . $title;
+      echo '</a>';
+    endforeach;
+  echo '</div>';
+else :
+  echo '<div class="ui error message"><p>Menu "' . $menu_name . '" not defined.</p></div>';
+endif;
+
+?>
       </header> <!-- header -->
