3360b45c456417e14e21bf2d010b6e92eafcdf21
Mirza Sisic <mirzasisic57@gmail.com>
2018-08-01 01:50:45 +0200

    Add more PHP Functions

--- index.php
+++ index.php
@@ -3,23 +3,35 @@
         <div class="hero">
            <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/hero.jpg" alt="" class="ui image">
             <div class="hero-overlay">
-              <p><?php bloginfo('name'); ?><span>Design and Development</span></p>
+              <p><?php bloginfo('name'); ?><span><?php bloginfo('description'); ?></span></p>
             </div> <!-- hero overlay -->
         </div> <!-- hero -->
     	
     	<div class="main-content index-main">
           <div class="ui container">
+            
             <div class="ui two column stackable grid">
-               <div class="column">
+              
+                <div class="column">
+                          <?php 
+
+                          if (have_posts()) :
+                            while (have_posts()) : the_post(); ?>
+
+                            <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+                            <?php the_content();?>
+
+                          <?php endwhile;
+
+                          else :
+                            echo '<p>No Content Found.</p>';
+
+                          endif;
+                            
+                          ?>
               </div> <!-- column -->
               <div class="middle aligned column">
-                <div class="home-text">
-                  <h4>Hey!, My Name is Gamaliel Rausch an independent Freelancer base in Berlin, Germany and I am an UI & UX Designer.I design and deliver creative collateral for some of the most world's most exclusive brands.</h4>
-                  
-                  <p>My clients porfolio spans a range of industries including interior design, property, jewellery and the arts. I combina a clear design aestheti with solid commercial rationale to create projects that truly inspire, inform and engage.</p>
-                  
-                  <p>This site gives you a bit of background about me and examples of <a href="work.html">my work</a>. If you have something in mind or are looking for something in particular, <a href="">get in touch</a> - that's what I am here for.</p>
-                </div> <!-- home-text -->
+                          <?php include('home_text.php'); ?>
               </div> <!-- column -->
             </div> <!-- grid -->
           </div> <!-- container -->
