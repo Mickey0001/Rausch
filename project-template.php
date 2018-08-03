2e758d1c8358d12354d67829f6df247b94bc53f0
Mirza Sisic <mirzasisic57@gmail.com>
2018-08-03 16:13:17 +0200

    Project Template Done

--- project-template.php
+++ project-template.php
@@ -8,7 +8,72 @@ Template Name: Project Template
 include('includes/header.php');
 ?>
   	
+  <div class="pusher">
+     <div class="wrapper">
+
+     
+     <div class="main-content">
+          <div class="ui container">
+            <div class="ui two column stackable grid">
+             <div class="middle aligned column">
+               <h1><?php the_title(); ?></h1>
+             </div> 
+              <div class="column">
+                <div class="home-text">
+                  <h3>My Clients</h3>
+                   <p>Branding, Digital, Print</p>
+                   
+                  <p>My clients porfolio spans a range of industries including interior design, property, jewellery and the arts. I combina a clear design aestheti with solid commercial rationale to create projects that truly inspire, inform and engage.</p>
+
+                  <p>This site gives you a bit of background about me and examples of <a href="work">my work</a>. If you have something in mind or are looking for something in particular, <a href="">get in touch</a> - that's what I am here for.</p>
+
+                  <p>
+                      <?php if(have_posts()) : ?>
+                          <?php while(have_posts()) : the_post(); ?>
+
+                          <?php the_content(); ?>
+                        </div><!-- /.blog-post -->
+                        <?php endwhile; ?>
+                      <?php else : ?>
+                        <p><?php __('No Page Found'); ?></p>
+                      <?php endif; ?>
+                  </p>
+                  <div class="text-links">
+                   <a href="http://www.pawpak.com/" target="_blank">www.pawpak.com</a>
+                  </div>
+                </div>
+              </div> <!-- column -->
+            </div> <!-- grid -->
+
+          <div class="sections">
+              <div class="devices">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/paw-pack.png" class="ui centered apple-device image">
+              </div> <!-- devices -->
+              <div class="path">
+                <div class="path-slieder">
+                  <img src="<?php bloginfo('template_url'); ?>/assets/images/paw-about.jpg"  class="ui image">
+                  <img src="<?php bloginfo('template_url'); ?>/assets/images/paw-2.jpg"  class="ui image">
+                  <img src="<?php bloginfo('template_url'); ?>/assets/images/paw-3.jpg"  class="ui image">
+                </div> <!-- path -->
+                <div class="arrows">
+                  <p class="left-angel"><i class="caret left icon"></i></p>
+                  <p class="right-angel"><i class="caret right icon"></i></p>
+                </div> <!-- arrows --->
+              </div> <!-- path -->
+             <div class="path">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/full-paw.jpg"  class="ui image">
+              </div> <!-- path -->
+              <div class="path">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/paw-wedding.jpg"  class="ui image">
+              </div> <!-- path -->
+            </div> <!-- sections -->
+          </div> <!-- container -->
+    	</div> <!-- main-content -->
 
 
-	<?php include('includes/footer.php'); ?>
+      </div> <!-- wrapper -->
+    </div> <!-- pusher -->
+  </div>
+
+<?php include('includes/footer.php'); ?>
 
