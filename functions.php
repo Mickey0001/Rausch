10b74e0fc894ddd21792e3c68391adbc618b697d
Mirza Sisic <mirzasisic57@gmail.com>
2018-08-16 02:01:21 +0200

    Testing the widget

--- functions.php
+++ functions.php
@@ -310,70 +310,60 @@ add_action('customize_register', 'themename_third_about_slider_setup');
 add_action('customize_register', 'themename_fourth_about_slider_setup');
 
 
+//Image Widget
+class Home_Rollover_Widget extends WP_Widget
+{
+
+  public function __construct()
+  {
+    parent::__construct(
+      'home-rollover-widget',
+      'Work Image Widget',
+      array(
+        'description' => 'Work Image Widget'
+      )
+    );
+  }
+
+  public function widget( $args, $instance )
+  {
+    // basic output just for this example
+    echo '<a href="#">
+      <img src="'.esc_url($instance['image_uri']).'" />
+      <h4>'.esc_html($instance['image_uri']).'</h4>
+    </a>';
+  }
+
+  public function form( $instance )
+  {
+    // removed the for loop, you can create new instances of the widget instead
+    ?>
+    <p>
+      <label for="<?php echo $this->get_field_id('text'); ?>">Text</label><br />
+      <input type="text" name="<?php echo $this->get_field_name('text'); ?>" id="<?php echo $this->get_field_id('text'); ?>" value="<?php echo $instance['text']; ?>" class="widefat" />
+    </p>
+    <p>
+      <label for="<?php echo $this->get_field_id('image_uri'); ?>">Image</label><br />
+      <input type="text" class="img" name="<?php echo $this->get_field_name('image_uri'); ?>" id="<?php echo $this->get_field_id('image_uri'); ?>" value="<?php echo $instance['image_uri']; ?>" />
+      <input type="button" class="select-img" value="Select Image" />
+    </p>
+    <?php
+  }
 
 
+} 
+// end class
 
-// Register and load the widget
-function wpb_load_widget() {
-    register_widget( 'wpb_widget' );
-}
-add_action( 'widgets_init', 'wpb_load_widget' );
- 
-// Creating the widget 
-class wpb_widget extends WP_Widget {
- 
-function __construct() {
-parent::__construct(
- 
-// Base ID of your widget
-'wpb_widget', 
- 
-// Widget name will appear in UI
-__('WPBeginner Widget', 'wpb_widget_domain'), 
- 
-// Widget description
-array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), ) 
-);
-}
- 
-// Creating widget front-end
- 
-public function widget( $args, $instance ) {
-$title = apply_filters( 'widget_title', $instance['title'] );
- 
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
-if ( ! empty( $title ) )
-echo $args['before_title'] . $title . $args['after_title'];
- 
-// This is where you run the code and display the output
-echo __( 'Hello, World!', 'wpb_widget_domain' );
-echo $args['after_widget'];
-}
-         
-// Widget Backend 
-public function form( $instance ) {
-if ( isset( $instance[ 'title' ] ) ) {
-$title = $instance[ 'title' ];
-}
-else {
-$title = __( 'New title', 'wpb_widget_domain' );
-}
-// Widget admin form
-?>
-<p>
-<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-</p>
-<?php 
-}
-     
-// Updating widget replacing old instances with new
-public function update( $new_instance, $old_instance ) {
-$instance = array();
-$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-return $instance;
-}
-} // Class wpb_widget ends here
+// init the widget
+add_action( 'widgets_init', create_function('', 'return register_widget("Home_Rollover_Widget");') );
 
-
+// queue up the necessary js
+function rausch_enqueue()
+{
+  wp_enqueue_style('thickbox');
+  wp_enqueue_script('media-upload');
+  wp_enqueue_script('thickbox');
+  // moved the js to an external file, you may want to change the path
+  wp_enqueue_script('widget.js', null, null, true);
+}
+add_action('admin_enqueue_scripts', 'rausch_enqueue');
