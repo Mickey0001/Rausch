35499381a9d458ac53643f713da257bc85921a8c
Mirza Sisic <mirzasisic57@gmail.com>
2018-08-17 19:43:16 +0200

    Image Widget

--- work_image_widget.php
+++ work_image_widget.php
@@ -8,8 +8,9 @@ class work_image_widget extends WP_Widget
 function work_image_widget() 
 
 {
+
 // Give widget name here
-parent::WP_Widget(false, $name = __('Work Image', 'work_image_widget.php') );
+parent::WP_Widget(false, $name = __('Work Image', 'work_image_widget') );
 
 }
 
@@ -19,22 +20,28 @@ function form($instance)
 {
 
   // Check values
-  if( $instance) {
+  if( $instance) 
+
+  {
   $title = esc_attr($instance['title']);
   $textarea = $instance['textarea'];
-  } else {
+  $image = $instance['image'];
+  } 
+  else 
+  {
   $title = '';
   $textarea = '';
+  $image = '';
   }
   ?>
   
   <p>
-  <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
+  <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'work_image_widget'); ?></label>
   <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
   </p>
   <p>
-  <label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Description:', 'wp_widget_plugin'); ?></label>
-  <textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>" rows="7" cols="20" ><?php echo $textarea; ?></textarea>
+  <label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Description:', 'work_image_widget'); ?></label>
+  <textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>" rows="1" cols="2" ><?php echo $textarea; ?></textarea>
   </p>
   <?php
   }
@@ -45,8 +52,39 @@ function form($instance)
     // Fields
     $instance['title'] = strip_tags($new_instance['title']);
     $instance['textarea'] = strip_tags($new_instance['textarea']);
+    $instance['image'] = strip_tags($new_instance['image']);
     return $instance;
     }
 
-}
+    // display widget
+function widget($args, $instance) {
+  extract( $args );
+  
+  // these are the widget options
+  $title = apply_filters('widget_title', $instance['title']);
+  $textarea = $instance['textarea'];
+  echo $before_widget;
+  
+  // Display the widget
+  echo '<div class="widget-text work_image_widget_box" style="width:269px; padding:5px 9px 20px 5px; border: 1px solid rgb(231, 15, 52); background: pink; border-radius: 5px; margin: 10px 0 25px 0;">';
+  echo '<div class="widget-title" style="width: 90%; height:30px; margin-left:3%; ">';
+  
+  // Check if title is set
+  if ( $title ) {
+  echo $before_title . $title . $after_title ;
+  }
+  echo '</div>';
+  
+  // Check if textarea is set
+  echo '<div class="widget-textarea" style="width: 90%; margin-left:3%; padding:8px; background-color: white; border-radius: 3px; min-height: 70px;">';
+  if( $textarea ) {
+  echo '<p class="work_image_widget_textarea" style="font-size:15px;">'.$textarea.'</p>';
+  }
+  echo '</div>';
+  echo '</div>';
+  echo $after_widget;
+  }
+  }
+
+  add_action('widgets_init', create_function('', 'return register_widget("work_image_widget");'));
 
