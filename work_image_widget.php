b4458afcb507ad47dc3bce5bdcbeca980e4622de
Mirza Sisic <mirzasisic57@gmail.com>
2018-08-22 02:38:44 +0200

    Working on image widget

--- work_image_widget.php
+++ work_image_widget.php
@@ -1,81 +1,64 @@
 <?php
-/**
- * Adds work_image widget.
- */
-class work_image extends WP_Widget
+
+class my_plugin extends WP_Widget
 {
 
-    /**
-     * Register widget with WordPress.
-     */
-    public function __construct()
+    // constructor
+    public function my_plugin()
     {
-        parent::__construct(
-            'work_image', // Base ID
-            esc_html__('Work Image Widget', 'text_domain'), // Name
-            array('description' => esc_html__('Work Image Widget', 'text_domain')) // Args
-        );
+        parent::WP_Widget(false, $name = __('My Image Widget', 'wp_widget_plugin'));
+
     }
 
-    /**
-     * Front-end display of widget.
-     *
-     * @see WP_Widget::widget()
-     *
-     * @param array $args     Widget arguments.
-     * @param array $instance Saved values from database.
-     */
-    public function widget($args, $instance)
-    {
-        echo $args['before_widget'];
-        if (!empty($instance['title'])) {
-            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
-        }
-        echo esc_html__('Hello, World!', 'text_domain');
-        echo $args['after_widget'];
-    }
-
-    /**
-     * Back-end widget form.
-     *
-     * @see WP_Widget::form()
-     *
-     * @param array $instance Previously saved values from database.
-     */
+// widget form creation
     public function form($instance)
     {
-        $title = !empty($instance['title']) ? $instance['title'] : esc_html__('New title', 'text_domain');
+
+// Check values
+        if ($instance) {
+            $textarea = $instance['textarea'];
+        } else {
+            $textarea = '';
+        }
         ?>
-		<p>
-		<label for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php esc_attr_e('Title:', 'text_domain');?></label>
-		<input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text" value="<?php echo esc_attr($title); ?>">
-		</p>
-		<?php
+
+<p>
+<label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Description:', 'wp_widget_plugin');?></label>
+<textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>" rows="1" cols="1" ><?php echo $textarea; ?></textarea>
+</p>
+
+<?php
 }
 
-    /**
-     * Sanitize widget form values as they are saved.
-     *
-     * @see WP_Widget::update()
-     *
-     * @param array $new_instance Values just sent to be saved.
-     * @param array $old_instance Previously saved values from database.
-     *
-     * @return array Updated safe values to be saved.
-     */
     public function update($new_instance, $old_instance)
     {
-        $instance = array();
-        $instance['title'] = (!empty($new_instance['title'])) ? sanitize_text_field($new_instance['title']) : '';
-
+        $instance = $old_instance;
+        // Fields
+        $instance['textarea'] = strip_tags($new_instance['textarea']);
         return $instance;
     }
 
-} // class work_image
+    // display widget
+    public function widget($args, $instance)
+    {
+        extract($args);
 
-// register work_image widget
-function register_work_image()
-{
-    register_widget('work_image');
+        // these are the widget options
+        $textarea = $instance['textarea'];
+        echo $before_widget;
+
+        // Display the widget
+
+        // Check if textarea is set
+        echo '<div class="info">';
+        if ($textarea) {
+            echo '<p><span>' . $textarea . '</span></p>';
+        }
+        echo '</div>';
+        echo $after_widget;
+    }
 }
-add_action('widgets_init', 'register_work_image');
+
+// register widget
+add_action('widgets_init', create_function('', 'return register_widget("my_plugin");'));
+?>
